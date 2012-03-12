7f1527077cd835e4d3879317e43723b3297a0808
Ubuntu <ubuntu@ip-10-170-18-110.us-west-1.compute.internal>
2012-03-12 05:40:09 +0000

    fixed an indentation problem in the haml that put the refresh button outside of the form tag, so it basically did nothing, also added compact() to the end of the ratings method in the model- this removes the nil at the end of the array and added order(:rating) to the same method so the ratings appear in order as G PG PG-13 R

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
 #Note that ratings is a class method here rather than instance method
   def Movie.ratings
-      return Movie.select(:rating).map(&:rating).uniq
+      return Movie.select(:rating).order(:rating).map(&:rating).uniq.compact
   end
 end
