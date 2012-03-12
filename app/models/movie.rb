6f2e98eb385b265a20282dc8463562885d0845f0
Ubuntu <ubuntu@ip-10-170-18-110.us-west-1.compute.internal>
2012-03-12 04:56:52 +0000

    initial integration of ratings checkboxes- added the checkboxes themselves to the view haml file and defined @all_movies in the controller to be the return value of a ratings method you added to the model which does the code below to return unique ratings: Movie.select(:rating).map(&:rating).uniq

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+#Note that ratings is a class method here rather than instance method
+  def Movie.ratings
+      return Movie.select(:rating).map(&:rating).uniq
+  end
 end
